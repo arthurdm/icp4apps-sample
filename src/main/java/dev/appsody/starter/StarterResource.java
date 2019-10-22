a14b45a62c69245c3bdf6fbfc593f5f4fa1eb801
Arthur De Magalhaes <ademagalhaes@gmail.com>
2019-10-22 13:10:49 -0400

    Initial drop
    Signed-off-by: Arthur De Magalhaes <ademagalhaes@gmail.com>

---
+++ src/main/java/dev/appsody/starter/StarterResource.java
@@ -0,0 +1,20 @@
+package dev.appsody.starter;
+
+import static javax.ws.rs.core.MediaType.TEXT_HTML;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@ApplicationScoped
+@Path("/")
+public class StarterResource {
+
+    @GET
+    @Produces(TEXT_HTML)
+    public String info() {
+        return "Microservice v1";
+    }
+
+}
